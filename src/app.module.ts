d80c761e9aa19a7ba5355c5d24bb86e72921bb8f
Amirul Lokman Jamaludin <amirul.l.jamaludin@gmail.com>
2023-04-23 22:22:13 +0800

    Add user module to root module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -9,6 +10,7 @@ import { DatabaseModule } from './database/database.module';
       isGlobal: true,
     }),
     DatabaseModule,
+    UsersModule,
   ],
   controllers: [],
   providers: [],
